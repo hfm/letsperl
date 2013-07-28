bf613eaf797ab3aee34305a6940f0e69a4f84999
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-07-29 08:45:45 +0900

    [WIP]2章

---
+++ learning-perl/chap2/except_decimal_literal.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+use 5.018;
+use warnings;
+
+say 0377;
+say 0xff;
+say 0b11111111;
+say 0b11111110;
+say 0b1111;
+say 0b11_11;
+
+# 255
+# 255
+# 255
+# 254
+# 15
+# 15
+# 
+# *** time: 0.189446 ***
